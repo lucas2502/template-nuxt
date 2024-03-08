a3fd49e3b6506b46533522609c7e83ddd2f93bdb
lucas2502 <lucas2502@live.com>
2024-03-08 18:35:40 -0300

    chore: call use case

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <template>
-  
+  <div></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import getByZipCodeUseCase from '~/logic/modules/address/use-cases/GetByZipCode';
+
+onMounted(async () => {
+  const res = await getByZipCodeUseCase.execute({
+    zipCode: '04750030'
+  });
+  if (res.isRight()) {
+    console.log(res.value.getValue());
+  }
+});
+</script>
 
 <style lang="scss" scoped></style>
